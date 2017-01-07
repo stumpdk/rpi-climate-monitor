9bc80d4c331c93f23f5e848f9f0e6dddde3d73b6
Bo Henriksen <stumpdenmark@hotmail.com>
2017-01-07 20:44:40 +0100

    First commit

---
+++ init/init.sql
@@ -0,0 +1,2 @@
+use Monitoring;
+create table TempHumid (ComputerTime INTEGER UNSIGNED,Temperature DECIMAL(5,1), Humidity DECIMAL(5,1));
---
+++ init/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+service mysql restart;
+service ssh restart;
+echo "create database Monitoring;" | mysql -u root;
+echo "use Monitoring; create table if not exists TempHumid (ComputerTime TIMESTAMP,Temperature DECIMAL(5,1), Humidity DECIMAL(5,1))"  | mysql -u root;
+
+/tmp/performMeasurement;
+cron;
+tail -f /var/log/mysql/error.log;
+while true; do sleep 2; done;
