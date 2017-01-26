de5f08a251323f850f91c58fe758392e458c08c2
StumpDK <stumpdenmark@hotmail.com>
2017-01-26 20:25:34 +0100

    Simplified entry point for measurer

--- start.sh
+++ start.sh
@@ -1,16 +1,2 @@
 #!/bin/sh
-# Start MySQL
-echo 'restarting MySQL';
-service mysql restart;
-#service ssh restart;
-
-# Create database and table
-echo 'creating database';
-echo "create database if not exists $MYSQL_DATABASE;" | mysql -u root;
-echo 'creating table';
-echo "use $MYSQL_DATABASE; create table if not exists TempHumid (ComputerTime TIMESTAMP,Temperature DECIMAL(5,1), Humidity DECIMAL(5,1))"  | mysql -u root;
-
-# Watch mysql error logs
-#tail -f /var/log/mysql/error.log;
-#touch /var/log/rpi-climate-monitor.log
-#tail -f /var/log/mysql/error.log
+tail -f /data/rpi-climate-monitor.log;
