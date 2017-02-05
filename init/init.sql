ad3c853028a39a262d93e617532301035d3d31bd
StumpDK <stumpdenmark@hotmail.com>
2017-02-05 23:39:26 +0100

    Added user in init.sq

--- init/init.sql
+++ init/init.sql
@@ -1,2 +1,5 @@
 create database if not exists `measurings`;
 create table if not exists `measurings`.`measurings` (`time` TIMESTAMP,`temperature` DECIMAL(5,1), `humidity` DECIMAL(5,1));
+#CREATE USER 'dev'@'%' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON *.* TO 'dev'@'%';
+FLUSH PRIVILEGES;
