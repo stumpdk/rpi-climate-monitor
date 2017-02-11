910dea1b754cffcbe22bf8fa2e4c24c9d59a41ac
StumpDK <stumpdenmark@hotmail.com>
2017-02-11 22:05:33 +0100

    added privileges

--- rpi-climate-monitor-db/init.sql
+++ rpi-climate-monitor-db/init.sql
@@ -1,5 +1,6 @@
 create database if not exists `measurings`;
 create table if not exists `measurings`.`measurings` (`time` TIMESTAMP,`temperature` DECIMAL(5,1), `humidity` DECIMAL(5,1));
-#CREATE USER 'dev'@'%' IDENTIFIED BY '123456';
+CREATE USER 'dev'@'%' IDENTIFIED BY '123456';
 GRANT ALL PRIVILEGES ON *.* TO 'dev'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
