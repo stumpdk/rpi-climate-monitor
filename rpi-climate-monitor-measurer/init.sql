164f1a9c6f02773fe430542b4511c928f7ea3236
StumpDK <stumpdenmark@hotmail.com>
2017-02-14 21:48:23 +0100

    Testing sql init

--- rpi-climate-monitor-measurer/init.sql
+++ rpi-climate-monitor-measurer/init.sql
@@ -1,1 +1,7 @@
-create table if not exists `measurings` (`time` TIMESTAMP,`temperature` DECIMAL(5,1), `humidity` DECIMAL(5,1));
+create table measurings (
+	ID INT PRIMARY KEY NOT NULL,
+	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	temperature DECIMAL(5,1), 
+	humidity DECIMAL(5,1)
+);	
+INSERT INTO measurings (`temperature`, `humidity`) VALUES(' 22.2',' 29.0');
